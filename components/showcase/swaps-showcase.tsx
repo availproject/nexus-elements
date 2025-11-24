f1ab62d6dc8a28bfd34832af1166e3904cd588ca
decocereus <amartyasinghkings07@gmail.com>
2025-11-24 16:52:41 +0530

    refactor: simplify SwapsShowcase component and remove ExperienceProvider

--- components/showcase/swaps-showcase.tsx
+++ components/showcase/swaps-showcase.tsx
@@ -1,20 +1,12 @@
-"use client";
-import React, { useState } from "react";
+import React from "react";
 import ShowcaseWrapper from "./showcase-wrapper";
 import Swaps from "@/registry/nexus-elements/swaps/swaps";
 
 const SwapsShowcase = () => {
-  const [swapAs, setSwapAs] = useState<"exactIn" | "exactOut">("exactIn");
   return (
     <ShowcaseWrapper
       connectLabel="Connect wallet to use Nexus Swaps"
       type="swaps"
-      pressed={swapAs === "exactIn"}
-      onPressedChange={(value: boolean) =>
-        setSwapAs(value ? "exactIn" : "exactOut")
-      }
-      toggle={true}
-      toggleLabel="Swap with Exact In"
     >
       <Swaps />
     </ShowcaseWrapper>
