10f27bb1ce821e8d90898d689a786ec1cb234dc3
decocereus <amartyasinghkings07@gmail.com>
2025-11-24 15:30:09 +0530

    feat: updated swap ui

--- registry/nexus-elements/swaps/swaps.tsx
+++ registry/nexus-elements/swaps/swaps.tsx
@@ -31,8 +31,8 @@ const Swaps = ({
 }: SwapsProps) => {
   return (
     <Tabs className="w-full" defaultValue={"exact-in"}>
-      <TabsList className="w-full">
-        <TabsTrigger value="exact-in">
+      <TabsList>
+        <TabsTrigger value="exact-int">
           <p className="text-sm font-medium">Exact In</p>
         </TabsTrigger>
         <TabsTrigger value="exact-out">
