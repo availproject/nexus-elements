2f9ac15c6f682dcdae63dee1164d115bb30546a8
dakshi goel <dakshiegoel@gmail.com>
2025-12-22 09:49:30 +0530

    showcase wrapper

--- components/showcase/new-deposit-showcase.tsx
+++ components/showcase/new-deposit-showcase.tsx
@@ -5,6 +5,7 @@ import NexusDeposit from "@/registry/nexus-elements/new-deposit/nexus-deposit";
 const NewDepositShowcase = () => {
   return (
     <ShowcaseWrapper
+      type="new-deposit"
       connectLabel="Connect wallet to use New Deposit Widget"
     >
       <NexusDeposit />
