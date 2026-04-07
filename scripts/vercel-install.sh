59961fc905d2e81ed691a9a8c8d4c34d45b969da
shrinathprabhu <shrinathprabhu.123@gmail.com>
2026-04-07 11:54:42 +0530

    Update the script to use GH_PAT

--- scripts/vercel-install.sh
+++ scripts/vercel-install.sh
@@ -2,13 +2,13 @@
 set -e
 
 echo "Configuring git auth..."
-git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "git@github.com:"
-git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "ssh://git@github.com/"
-git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
+git config --global url."https://${GH_PAT}@github.com/".insteadOf "git@github.com:"
+git config --global url."https://${GH_PAT}@github.com/".insteadOf "ssh://git@github.com/"
+git config --global url."https://${GH_PAT}@github.com/".insteadOf "https://github.com/"
 
-echo "Verifying GITHUB_TOKEN is set..."
-if [ -z "$GITHUB_TOKEN" ]; then
-  echo "ERROR: GITHUB_TOKEN is not set"
+echo "Verifying GH_PAT is set..."
+if [ -z "$GH_PAT" ]; then
+  echo "ERROR: GH_PAT is not set"
   exit 1
 fi
 
