a55fc5caa7e9a3cad535fc414a90623408656575
Abhishek <abhishek.chaudhary.2008@gmail.com>
2026-04-07 13:58:12 +0400

    fix: frozen lockfile but allow build script

--- scripts/vercel-install.sh
+++ scripts/vercel-install.sh
@@ -8,4 +8,4 @@ git config --global --add url."${REWRITE}".insteadOf "git@github.com:"
 git config --global --add url."${REWRITE}".insteadOf "ssh://git@github.com/"
 git config --global --add url."${REWRITE}".insteadOf "https://github.com/"
 
-pnpm install --frozen-lockfile --ignore-scripts --reporter=append-only
+pnpm install --frozen-lockfile --reporter=append-only
