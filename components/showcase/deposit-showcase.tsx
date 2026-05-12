25454464792e5dfe9442eb45ed3c7607d5a5b6cb
shrinathprabhu <shrinathprabhu.123@gmail.com>
2026-05-12 16:18:18 +0530

    Add wcBTC on Zentra

--- components/showcase/deposit-showcase.tsx
+++ components/showcase/deposit-showcase.tsx
@@ -31,24 +31,28 @@ const DepositShowcase = () => {
       {
         inputs: [
           {
+            internalType: "address",
+            name: "asset",
+            type: "address",
+          },
+          {
             internalType: "uint256",
-            name: "assets",
+            name: "amount",
             type: "uint256",
           },
           {
             internalType: "address",
-            name: "onBehalf",
+            name: "onBehalfOf",
             type: "address",
           },
-        ],
-        name: "deposit",
-        outputs: [
           {
-            internalType: "uint256",
-            name: "",
-            type: "uint256",
+            internalType: "uint16",
+            name: "referralCode",
+            type: "uint16",
           },
         ],
+        name: "supply",
+        outputs: [],
         stateMutability: "nonpayable",
         type: "function",
       },
@@ -56,8 +60,8 @@ const DepositShowcase = () => {
 
     const encoded = encodeFunctionData({
       abi: abi,
-      functionName: "deposit",
-      args: [amount, user],
+      functionName: "supply",
+      args: [tokenAddress, amount, user, 0],
     });
     return {
       to: contractAddress,
@@ -82,21 +86,22 @@ const DepositShowcase = () => {
     >
       <NexusDeposit
         embed={embed}
-        heading={"Deposit ctUSD on Mystic on Citrea"}
+        heading={"Deposit wcBTC on Zentra on Citrea"}
         destination={{
           chainId: SUPPORTED_CHAINS.CITREA,
-          tokenAddress: "0x8D82c4E3c936C7B5724A382a9c5a4E6Eb7aB6d5D",
-          tokenSymbol: "ctUSD",
-          tokenDecimals: 6,
-          tokenLogo: "https://files.availproject.org/nexus-elements/ctUSD.svg",
-          label: "Deposit ctUSD on Mystic on Citrea",
+          tokenAddress: "0x3100000000000000000000000000000000000006",
+          tokenSymbol: "wcBTC",
+          tokenDecimals: 18,
+          tokenLogo:
+            "https://assets.coingecko.com/coins/images/102172843/standard/cBTC.png?1776227743",
+          label: "Deposit wcBTC on Zentra on Citrea",
           gasTokenSymbol:
             CHAIN_METADATA[SUPPORTED_CHAINS.CITREA].nativeCurrency.symbol,
           estimatedTime: "≈ 30s",
           explorerUrl:
             CHAIN_METADATA[SUPPORTED_CHAINS.CITREA].blockExplorerUrls[0],
           depositTargetLogo:
-            "https://files.availproject.org/nexus-elements/mystic.png",
+            "https://zentrafinance.gitbook.io/zentra/~gitbook/image?url=https%3A%2F%2F2899070418-files.gitbook.io%2F%7E%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Forganizations%252F1jzW9aBSq190MuRJKgIj%252Fsites%252Fsite_2l6Ro%252Ficon%252Fb8adwB6RA7Y6VJH3vGjh%252FZentra%2520%284%29.png%3Falt%3Dmedia%26token%3D8aa44578-e817-4c2f-b20e-abd25827d4fe&width=32&dpr=3&quality=100&sign=d18163fe&sv=2",
         }}
         executeDeposit={executeDeposit}
       />
